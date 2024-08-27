5f58af238c1ec81cad25db771e21e069a7872104
Vivek <vivekchikamntr@gmail.com>
2024-08-27 16:06:40 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from calc_func import do_addition, do_subtraction
+
+def main():
+  print("Welcome to the calculator app")
+  print("""\nSelect the function from the given options
+        1. Add
+        2. Subtract
+        """)
+  
+  user_input = input("Select the function ")
+  
+  a = int(input("Value of A "))
+  b = int(input("Value of B "))
+  
+  if user_input == "1":
+    result = do_addition(a, b)
+  elif user_input == "2":
+    result = do_subtraction(a, b)
+    
+  print("Result :", result)
+  
+  
+if __name__ == "__main__":
+  main()
